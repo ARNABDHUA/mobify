91e27c782dd2006c47607ca763b8648919d0598b
ARNABDHUA <arnabdhua74@gmail.com>
2024-10-19 10:28:35 +0530

    login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,14 +4,14 @@ import ok from '../../../public/ok.gif'
 const Login = () => {
   return (
     <div>
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center min-h-screen">
       <div
-        className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
+        className="relative flex flex-col m-6 space-y-8 bg-white dark:bg-gray-400 shadow-2xl rounded-2xl md:flex-row md:space-y-0"
       >
         {/* <!-- left side --> */}
         <div className="flex flex-col justify-center p-8 md:p-14">
           <span className="mb-3 text-4xl font-bold">Welcome back</span>
-          <span className="font-light text-gray-400 mb-8">
+          <span className="font-light mb-8">
             Welcom back! Please enter your details
           </span>
           <div className="py-4">
@@ -50,7 +50,7 @@ const Login = () => {
             <img src={google} alt="img" className="w-6 h-6 inline mr-2" />
             Sign in with Google
           </button>
-          <div className="text-center text-gray-400">
+          <div className="text-center ">
             Dont'have an account?
             <span className="font-bold text-black">Sign up for free</span>
           </div>
